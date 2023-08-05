b97ca3a991e2a32f1993a1b1859dfead5c9489b3
danielvillam <jvillame@unal.edu.co>
2023-08-05 13:27:02 -0500

    Week 1 of the course, Assignment 4: Finding the Closest Earthquakes to a Location

--- Week1/SearchingEarthquakeDataStarterProgram/ClosestQuakes.java
+++ Week1/SearchingEarthquakeDataStarterProgram/ClosestQuakes.java
@@ -9,23 +9,38 @@
 import java.util.*;
 
 public class ClosestQuakes {
+    private QuakeEntry findTheClosestQuake (ArrayList<QuakeEntry> list, Location from) {
+        QuakeEntry closest = list.get(0);
+        for (int k = 1; k < list.size(); k++) {
+            if (list.get(k).getLocation().distanceTo(from) < closest.getLocation().distanceTo(from))
+                closest = list.get(k);
+        }
+        return closest;
+    }
+
     public ArrayList<QuakeEntry> getClosest(ArrayList<QuakeEntry> quakeData, Location current, int howMany) {
         ArrayList<QuakeEntry> ret = new ArrayList<QuakeEntry>();
-        // TO DO
+        ArrayList<QuakeEntry> in = new ArrayList<QuakeEntry>(quakeData);
 
+        while (howMany > 0 && ! in.isEmpty()) {
+            QuakeEntry closest = findTheClosestQuake(in, current);
+            ret.add(closest);
+            in.remove(closest);
+            howMany--;
+        }
         return ret;
     }
 
     public void findClosestQuakes() {
         EarthQuakeParser parser = new EarthQuakeParser();
-        //String source = "data/nov20quakedata.atom";
-        String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        String source = "data/nov20quakedatasmall.atom";
+        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
         ArrayList<QuakeEntry> list  = parser.read(source);
         System.out.println("read data for "+list.size());
 
         Location jakarta  = new Location(-6.211,106.845);
 
-        ArrayList<QuakeEntry> close = getClosest(list,jakarta,10);
+        ArrayList<QuakeEntry> close = getClosest(list,jakarta,3);
         for(int k=0; k < close.size(); k++){
             QuakeEntry entry = close.get(k);
             double distanceInMeters = jakarta.distanceTo(entry.getLocation());
