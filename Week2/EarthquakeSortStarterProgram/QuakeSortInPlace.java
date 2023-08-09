c8b6a0f33e8048d8e2c19ede5ea9563897621c10
danielvillam <jvillame@unal.edu.co>
2023-08-09 10:45:23 -0500

    Week 2 of the course, Assignment 1: Sort by Depth

---
+++ Week2/EarthquakeSortStarterProgram/QuakeSortInPlace.java
@@ -0,0 +1,92 @@
+/** 
+ * @author (https://github.com/danielvillam) 
+ * @version (August 9, 2023)
+ */
+
+import java.util.*;
+import edu.duke.*;
+
+public class QuakeSortInPlace {
+    public QuakeSortInPlace() {
+        // TODO Auto-generated constructor stub
+    }
+   
+    public int getSmallestMagnitude(ArrayList<QuakeEntry> quakes, int from) {
+        int minIdx = from;
+        for (int i=from+1; i< quakes.size(); i++) {
+            if (quakes.get(i).getMagnitude() < quakes.get(minIdx).getMagnitude()) {
+                minIdx = i;
+            }
+        }
+        return minIdx;
+    }
+    
+    public void sortByMagnitude(ArrayList<QuakeEntry> in) {
+       
+       for (int i=0; i< in.size(); i++) {
+            int minIdx = getSmallestMagnitude(in,i);
+            QuakeEntry qi = in.get(i);
+            QuakeEntry qmin = in.get(minIdx);
+            in.set(i,qmin);
+            in.set(minIdx,qi);
+        }
+        
+    }
+
+    public void testSort() {
+        EarthQuakeParser parser = new EarthQuakeParser(); 
+        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        String source = "data/nov20quakedatasmall.atom";
+        //String source = "data/nov20quakedata.atom";
+        ArrayList<QuakeEntry> list  = parser.read(source);  
+       
+        System.out.println("read data for "+list.size()+" quakes");    
+        //sortByMagnitude(list);
+        sortByLargestDepth(list);
+        for (QuakeEntry qe: list) { 
+            System.out.println(qe);
+        } 
+        
+    }
+    
+    public void createCSV() {
+        EarthQuakeParser parser = new EarthQuakeParser();
+        //String source = "data/nov20quakedata.atom";
+        String source = "data/nov20quakedatasmall.atom";
+        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        ArrayList<QuakeEntry> list  = parser.read(source);
+        dumpCSV(list);
+        System.out.println("# quakes read: " + list.size());
+    }
+    
+    public void dumpCSV(ArrayList<QuakeEntry> list) {
+        System.out.println("Latitude,Longitude,Magnitude,Info");
+        for(QuakeEntry qe : list){
+            System.out.printf("%4.2f,%4.2f,%4.2f,%s\n",
+                              qe.getLocation().getLatitude(),
+                              qe.getLocation().getLongitude(),
+                              qe.getMagnitude(),
+                              qe.getInfo());
+        }
+    }
+    
+    public int getLargestDepth (ArrayList<QuakeEntry> quakeData, int from){
+        int largestIdx = from;
+        for (int i=from+1; i< quakeData.size(); i++) {
+            if (quakeData.get(i).getDepth() > quakeData.get(largestIdx).getDepth()) {
+                largestIdx = i;
+            }
+        }
+        return largestIdx;
+    }
+    
+    public void sortByLargestDepth (ArrayList<QuakeEntry> in){
+        for (int i=0; i< in.size(); i++) {
+            int largIdx = getLargestDepth(in,i);
+            QuakeEntry qi = in.get(i);
+            QuakeEntry qLarg = in.get(largIdx);
+            in.set(i,qLarg);
+            in.set(largIdx,qi);
+        }
+    }
+}
