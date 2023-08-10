4b29acf2cf97ea4599cf0ca488555189f7b95d79
danielvillam <jvillame@unal.edu.co>
2023-08-10 11:34:43 -0500

    Week 2 of the course, Assignment 1: compareTo Method

---
+++ Week2/EfficientSortStarterProgram/MagnitudeComparator.java
@@ -0,0 +1,13 @@
+/** 
+ * @author (https://github.com/danielvillam) 
+ * @version (August 10, 2023)
+ */
+
+import java.util.*;
+
+public class MagnitudeComparator implements Comparator<QuakeEntry> {
+    public int compare(QuakeEntry qe1, QuakeEntry qe2) {
+        return Double.compare(qe1.getMagnitude(), qe2.getMagnitude());
+    }
+    
+}
