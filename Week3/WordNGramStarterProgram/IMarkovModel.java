84418678608955ed890d0a7d89fda930aff7e8e7
danielvillam <jvillame@unal.edu.co>
2023-08-14 18:21:19 -0500

    Week 3 of the course, Assignment 1: Generating Random Words with Prediction

---
+++ Week3/WordNGramStarterProgram/IMarkovModel.java
@@ -0,0 +1,13 @@
+/** 
+ * @author (https://github.com/danielvillam) 
+ * @version (August 14, 2023)
+ */
+
+public interface IMarkovModel {
+    public void setTraining(String text);
+    
+    public void setRandom(int seed);
+    
+    public String getRandomText(int numChars);
+
+}
